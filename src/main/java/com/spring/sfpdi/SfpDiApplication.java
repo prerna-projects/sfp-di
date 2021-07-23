83e6099b5ff3dd89cf32361ece7974e4b71a3a85
Prerna-projects <prernagoyal24@gmail.com>
2021-07-23 08:06:30 +0530

    inital commit

---
+++ src/main/java/com/spring/sfpdi/SfpDiApplication.java
@@ -0,0 +1,13 @@
+package com.spring.sfpdi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SfpDiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SfpDiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/spring/sfpdi/SfpDiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.sfpdi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SfpDiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
